5088402b4e1b2a8042eb5feb050a2d63ff0790f6
Mikhil Jose <6015616+mikhy888@users.noreply.github.com>
2020-04-11 17:05:54 +0530

    updated with ajax form submission

--- form.php
+++ form.php
@@ -1,41 +1,4 @@
-<?php
-
-  $name = $email = $phone = $message ="";
-
-  function test_input($data) {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-  }
-
-  if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $firstname = test_input($_POST["name"]);
-    $email = test_input($_POST["email"]);
-    $phone = test_input($_POST["phone"]);
-    $message = test_input($_POST["message"]);
-
-    $messagemain = 'Name: ' . $firstname ."\r\n" .
-      'Email: ' . $email ."\r\n" .
-      'Phone Number: ' . $phone ."\r\n" .
-      'Message: ' . $message ;
-
-    $to      = 'gmail@gmail.com';
-    $subject = 'Website Message';
-    $message = $messagemain;
-    $headers = 'From: info@website.com' . "\r\n" .
-        'Reply-To: info@website.com' . "\r\n" .
-        'X-Mailer: PHP/' . phpversion();
-
-    mail($to, $subject, $messagemain, $headers);
-
-        header("location:thankyou.php");
-  }
-?>
-
-
-
-<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"  class="formmain" method="post" enctype="multipart/form-data" id="c-form">
+<form action="#"  class="formmain" method="post" enctype="multipart/form-data" id="c-form">
                       <div class="form-field">
                           <input type="text" placeholder="Name" name="name">
                           <div class="error"></div>
